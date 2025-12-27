55caeea12abcbb7185a5a3a74d869d924d891aca
Praful-B <baleraoprafulchandra2006@gmail.com>
2025-12-27 12:47:10 +0530

    testing code

--- src/main/java/org/backend/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/org/backend/expensetracker/ExpenseTrackerApplication.java
@@ -2,12 +2,21 @@ package org.backend.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration; // added for debugging
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExpenseTrackerApplication.class, args);
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
     }
 
 }
